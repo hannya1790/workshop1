94870eaac3bb69cc066cd463802871f3b21846f8
hannya1790 <161548131+hannya1790@users.noreply.github.com>
2024-05-16 16:09:38 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let NewItem = 100
+
+if (NewItem < 50)
+    {console.log('< 50') }
+
+else if(NewItem > 20)
+    {console.log('>20')}
+else {
+    console.log('error')
+}
+
+let NewItem1 = "mariam"
+
+if (NewItem1 == "mariam") {
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+
+let
